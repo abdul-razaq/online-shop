45ce766253e628cf90205a2e9909ff37a71f85f8
AbdulRazaq Suleiman <razaqayomide01@gmail.com>
2021-05-25 07:36:43 +0100

    refactored screens into logical folders

---
+++ screens/shop/ProductsOverviewScreen.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { FlatList, View, Text, StyleSheet } from "react-native";
+
+export default function ProductsOverviewScreen(props) {
+	return (
+		<View>
+			<Text>Products Overview Screen!</Text>
+			<FlatList />
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({});
