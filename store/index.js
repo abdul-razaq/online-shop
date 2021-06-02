af98d48ba802db5411620d2021053bef4742ce8d
AbdulRazaq Suleiman <razaqayomide01@gmail.com>
2021-06-02 23:01:50 +0100

    added orders action and reducers and then added the orders state slice to the entire root store

--- store/index.js
+++ store/index.js
@@ -2,8 +2,10 @@ import { combineReducers } from "redux";
 
 import productsReducer from "./reducers/products";
 import cartReducer from "./reducers/cart";
+import ordersReducer from "./reducers/orders";
 
 export default combineReducers({
 	products: productsReducer,
 	cart: cartReducer,
+	orders: ordersReducer,
 });
--- store/reducers/orders.js
+++ store/reducers/orders.js
@@ -1,14 +1,26 @@
-import { actionTypes } from '../actions/orders';
+import { actionTypes } from "../actions/orders";
+
+import Order from "../../models/orders";
 
 const initialState = {
-  orders: [],
+	orders: [],
 };
 
 export default function ordersReducer(state = initialState, { type, payload }) {
-  switch (type) {
-    case actionTypes.ADD_ORDER:
-      
-    default:
-      return state;
-  }
+	switch (type) {
+		case actionTypes.ADD_ORDER:
+			const { cartItems, totalAmount } = payload;
+			const newOrder = new Order(
+				new Date().toString(),
+				cartItems,
+				totalAmount,
+				new Date()
+			);
+			return {
+				...state,
+				orders: state.orders.concat(newOrder),
+			};
+		default:
+			return state;
+	}
 }
