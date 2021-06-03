852e16292887e7870eb56c99fb4141f694da95cc
AbdulRazaq Suleiman <razaqayomide01@gmail.com>
2021-06-03 11:39:17 +0100

    added reusable styles folder

---
+++ constants/Styles.js
@@ -0,0 +1,9 @@
+export const cardStyle = {
+	borderRadius: 10,
+	shadowColor: "#000",
+	shadowOpacity: 0.26,
+	shadowRadius: 10,
+	shadowOffset: { height: 2, width: 0 },
+	elevation: 8,
+	backgroundColor: "#fff",
+};
